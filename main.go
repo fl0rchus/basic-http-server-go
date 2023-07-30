3d3226f8af3505039e47d291762f4c50874840c3
fl0rchus <quinterosflorencia0@gmail.com>
2023-07-30 14:19:50 -0300

    feat: throw an error when the method is not GET

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method != http.MethodGet{
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintf(w, "Method not allowed")
+			return
+		}
+
 		fmt.Fprintf(w, "Hello")
 	})
 
